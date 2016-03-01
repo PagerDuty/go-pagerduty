a6d785dad7d8882f6a9b9c6ef44b42434deb07a7
Ranjib Dey <ranjib@linux.com>
2016-03-01 15:17:45 -0800

    implement notification api

---
+++ notification.go
@@ -0,0 +1,39 @@
+package pagerduty
+
+import (
+	"github.com/google/go-querystring/query"
+)
+
+type Notification struct {
+	ID        string `json:"id"`
+	Type      string
+	StartedAt string `json:"started_at"`
+	Address   string
+	User      APIObject
+}
+
+type ListNotificationOptions struct {
+	TimeZone string   `url:"time_zone,omitempty"`
+	Since    string   `url:"since,omitempty"`
+	Until    string   `url:"until,omitempty"`
+	Filter   string   `url:"filter,omitempty"`
+	Includes []string `url:"include,omitempty"`
+}
+
+type ListNotificationsResponse struct {
+	APIListObject
+	Notifications []Notification
+}
+
+func (c *Client) ListNotifications(o ListNotificationOptions) (*ListNotificationsResponse, error) {
+	v, err := query.Values(o)
+	if err != nil {
+		return nil, err
+	}
+	resp, err := c.Get("/notifications?" + v.Encode())
+	if err != nil {
+		return nil, err
+	}
+	var result ListNotificationsResponse
+	return &result, c.decodeJson(resp, &result)
+}
