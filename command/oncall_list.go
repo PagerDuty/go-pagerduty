2e7457692885c86c649ca274cf690f5db5af481b
Alastair Firth <afirth@users.noreply.github.com>
2016-09-06 02:29:01 +1000

    Support the /oncalls endpoint in the CLI (#24)
    * preliminary support for oncall list
    
    * fmt.Println already inserts blanks between operands
    
    * the implementation of this print is inconsistent (i, i+1) and superfluous. print valid yaml instead
    
    * support all args for oncall_list
    
    * add meta help string for oncall_list args

--- command/oncall_list.go
+++ command/oncall_list.go
@@ -1,11 +1,17 @@
 package main
 
 import (
-	"github.com/mitchellh/cli"
+	"fmt"
 	"strings"
+
+	"github.com/PagerDuty/go-pagerduty"
+	log "github.com/Sirupsen/logrus"
+	"github.com/mitchellh/cli"
+	"gopkg.in/yaml.v2"
 )
 
 type OncallList struct {
+	Meta
 }
 
 func OncallListCommand() (cli.Command, error) {
@@ -14,7 +20,9 @@ func OncallListCommand() (cli.Command, error) {
 
 func (c *OncallList) Help() string {
 	helpText := `
-	`
+	pd oncall list List on-calls
+
+	` + c.Meta.Help()
 	return strings.TrimSpace(helpText)
 }
 
@@ -23,5 +31,55 @@ func (c *OncallList) Synopsis() string {
 }
 
 func (c *OncallList) Run(args []string) int {
+	var escalationPolicyIDs []string
+	var includes []string
+	var scheduleIDs []string
+	var timeZone string
+	var userIDs []string
+	var until string
+	var since string
+	var earliest bool
+
+	flags := c.Meta.FlagSet("on-call list")
+	flags.Usage = func() { fmt.Println(c.Help()) }
+	flags.Var((*ArrayFlags)(&includes), "include", "Additional details to include (can be specified multiple times)")
+	flags.Var((*ArrayFlags)(&scheduleIDs), "schedule-id", "Only show for schedule ID (can be specified multiple times)")
+	flags.Var((*ArrayFlags)(&userIDs), "user-id", "Only show for user ID (can be specified multiple times)")
+	flags.Var((*ArrayFlags)(&escalationPolicyIDs), "escalationPolicy-id", "Only show for escalationPolicy ID (can be specified multiple times)")
+	flags.StringVar(&timeZone, "time-zone", "", "Time Zone")
+	flags.StringVar(&until, "until", "", "End of the time range over which you want to search")
+	flags.StringVar(&since, "since", "", "Start of the time range over which you want to search")
+	flags.BoolVar(&earliest, "earliest", false, "Return only the earliest on-call for each combination of escalation policy, escalation level, and user")
+
+	if err := flags.Parse(args); err != nil {
+		log.Error(err)
+		return -1
+	}
+	if err := c.Meta.Setup(); err != nil {
+		log.Error(err)
+		return -1
+	}
+	client := c.Meta.Client()
+	opts := pagerduty.ListOnCallOptions{
+		UserIDs:             userIDs,
+		Includes:            includes,
+		TimeZone:            timeZone,
+		EscalationPolicyIDs: escalationPolicyIDs,
+		ScheduleIDs:         scheduleIDs,
+		Until:               until,
+		Since:               since,
+		Earliest:            earliest,
+	}
+	if oncs, err := client.ListOnCalls(opts); err != nil {
+		log.Error(err)
+		return -1
+	} else {
+		data, err := yaml.Marshal(oncs.OnCalls)
+		if err != nil {
+			log.Error(err)
+			return -1
+		}
+		fmt.Println(string(data))
+	}
 	return 0
 }
