01b804f7d8c4834f212b917d61d3448dae5f24f4
Ranjib Dey <dey.ranjib@gmail.com>
2016-09-28 10:05:53 -0700

    Add test. fix version issue (#40)

--- command/main.go
+++ command/main.go
@@ -7,7 +7,7 @@ import (
 )
 
 const (
-	version = "0.1"
+	version = "0.0.0"
 )
 
 func loadCommands() map[string]cli.CommandFactory {
@@ -96,18 +96,14 @@ func loadCommands() map[string]cli.CommandFactory {
 }
 
 func main() {
-	os.Exit(invokeCLI())
+	os.Exit(invokeCLI(os.Args[1:]))
 }
 
-func invokeCLI() int {
-	args := os.Args[1:]
+func invokeCLI(args []string) int {
 	for _, arg := range args {
 		if arg == "-v" || arg == "--version" {
-			newArgs := make([]string, len(args)+1)
-			newArgs[0] = "version"
-			copy(newArgs[1:], args)
-			args = newArgs
-			break
+			fmt.Println(version)
+			return 0
 		}
 	}
 
