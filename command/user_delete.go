e2ea40e48fe87489029352347354c85fb740708d
KeisukeYamashita <19yamashita15@gmail.com>
2019-10-13 07:20:11 +0900

    add(user) delete user command

--- command/user_delete.go
+++ command/user_delete.go
@@ -1,11 +1,15 @@
 package main
 
 import (
-	"github.com/mitchellh/cli"
+	"fmt"
 	"strings"
+
+	"github.com/mitchellh/cli"
+	log "github.com/sirupsen/logrus"
 )
 
 type UserDelete struct {
+	Meta
 }
 
 func UserDeleteCommand() (cli.Command, error) {
@@ -14,6 +18,7 @@ func UserDeleteCommand() (cli.Command, error) {
 
 func (c *UserDelete) Help() string {
 	helpText := `
+	pd user delete <USER_ID> Remove an existing user
 	`
 	return strings.TrimSpace(helpText)
 }
@@ -23,5 +28,20 @@ func (c *UserDelete) Synopsis() string {
 }
 
 func (c *UserDelete) Run(args []string) int {
+	var userID string
+	flags := c.Meta.FlagSet("user delete")
+	flags.StringVar(&userID, "id", "", "User ID to remove")
+	flags.Usage = func() { fmt.Println(c.Help()) }
+	if err := flags.Parse(args); err != nil {
+		log.Error(err)
+		return -1
+	}
+
+	client := c.Meta.Client()
+	if err := client.DeleteUser(userID); err != nil {
+		log.Error(err)
+		return -1
+	}
+	log.Infof("Delete user %d", userID)
 	return 0
 }
--- go.mod
+++ go.mod
@@ -2,6 +2,7 @@ module github.com/PagerDuty/go-pagerduty
 
 require (
 	github.com/google/go-querystring v1.0.0
+	github.com/hashicorp/consul v1.6.1
 	github.com/mitchellh/cli v1.0.0
 	github.com/mitchellh/go-homedir v1.1.0
 	github.com/sirupsen/logrus v1.4.2
