e9c0a564bf2af1058a397563b07db29be5899ca4
KeisukeYamashita <19yamashita15@gmail.com>
2019-10-13 07:15:09 +0900

    add(user): create user command

--- command/user_create.go
+++ command/user_create.go
@@ -1,11 +1,16 @@
 package main
 
 import (
-	"github.com/mitchellh/cli"
+	"fmt"
 	"strings"
+
+	"github.com/PagerDuty/go-pagerduty"
+	"github.com/mitchellh/cli"
+	log "github.com/sirupsen/logrus"
 )
 
 type UserCreate struct {
+	Meta
 }
 
 func UserCreateCommand() (cli.Command, error) {
@@ -14,6 +19,7 @@ func UserCreateCommand() (cli.Command, error) {
 
 func (c *UserCreate) Help() string {
 	helpText := `
+	pd user create <EMAIL> Create a user
 	`
 	return strings.TrimSpace(helpText)
 }
@@ -23,5 +29,24 @@ func (c *UserCreate) Synopsis() string {
 }
 
 func (c *UserCreate) Run(args []string) int {
+	var email string
+	flags := c.Meta.FlagSet("user create")
+	flags.StringVar(&email, "email", "", "Email of the user")
+	flags.Usage = func() { fmt.Println(c.Help()) }
+	if err := flags.Parse(args); err != nil {
+		log.Error(err)
+		return -1
+	}
+	client := c.Meta.Client()
+	u := pagerduty.User{
+		Email: email,
+	}
+	if len(flags.Args()) != 1 {
+	}
+	if _, err := client.CreateUser(u); err != nil {
+		log.Error(err)
+		return -1
+	}
+
 	return 0
 }
--- go.mod
+++ go.mod
@@ -7,3 +7,5 @@ require (
 	github.com/sirupsen/logrus v1.4.2
 	gopkg.in/yaml.v2 v2.2.4
 )
+
+go 1.13
