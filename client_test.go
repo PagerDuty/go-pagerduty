c6d8462c22d77270e981ba13cb6257d92ff07cc0
Scott McAllister <smcallister@pagerduty.com>
2019-10-24 10:07:44 -0700

    wip starting tests

---
+++ client_test.go
@@ -0,0 +1,34 @@
+package pagerduty
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+var (
+	// mux is the HTTP request multiplexer used with the test server.
+	mux *http.ServeMux
+
+	// client is the PagerDuty client being tested.
+	client *Client
+
+	// server is a test HTTP server used to provide mock API responses.
+	server *httptest.Server
+)
+
+func setup() {
+	mux = http.NewServeMux()
+	server = httptest.NewServer(mux)
+	client, _ = NewClient(&Client{apiEndpoint: server.URL, Token: "foo"})
+}
+
+func teardown() {
+	server.Close()
+}
+
+func testMethod(t *testing.T, r *http.Request, want string) {
+	if got := r.Method; got != want {
+		t.Errorf("Request method: %v, want %v", got, want)
+	}
+}
