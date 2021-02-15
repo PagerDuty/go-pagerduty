e8ab954e9fd4de0d6a3ddb21bead8637941146d1
Chris <chrisevans@monzo.com>
2021-02-15 13:18:28 +0000

    Store ep as pointer

--- incident.go
+++ incident.go
@@ -154,12 +154,12 @@ type CreateIncidentOptions struct {
 
 // ManageIncidentsOptions is the structure used when PUTing updates to incidents to the ManageIncidents func
 type ManageIncidentsOptions struct {
-	ID               string           `json:"id"`
-	Type             string           `json:"type"`
-	Status           string           `json:"status,omitempty"`
-	Priority         *APIReference    `json:"priority,omitempty"`
-	Assignments      []Assignee       `json:"assignments,omitempty"`
-	EscalationPolicy EscalationPolicy `json:"escalation_policy,omitempty"`
+	ID               string            `json:"id"`
+	Type             string            `json:"type"`
+	Status           string            `json:"status,omitempty"`
+	Priority         *APIReference     `json:"priority,omitempty"`
+	Assignments      []Assignee        `json:"assignments,omitempty"`
+	EscalationPolicy *EscalationPolicy `json:"escalation_policy,omitempty"`
 }
 
 // MergeIncidentsOptions is the structure used when merging incidents with MergeIncidents func
