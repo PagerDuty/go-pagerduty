f41ecbbc55b9c47abc09ba911697e23d8f3fffca
Todd <todd@blockpane.com>
2021-02-19 23:22:09 -0700

    return nil instead of resp on error

--- event.go
+++ event.go
@@ -49,7 +49,7 @@ func CreateEventWithHTTPClient(e Event, client HTTPClient) (*EventResponse, erro
 	req.Header.Set("Content-Type", "application/json")
 	resp, err := client.Do(req)
 	if err != nil {
-		return &EventResponse{HttpStatus: resp.StatusCode}, err
+		return  nil, fmt.Errorf("failed to action request: %w", err)
 	}
 	defer resp.Body.Close()
 	if resp.StatusCode != http.StatusOK {
