1f7bbf5315f94187acd9f89d1bc90ebc11930f89
José Antonio Reyes <imjaroiswebdev@gmail.com>
2024-01-10 19:12:32 -0300

    handle token refresh for a few known cases

--- token_source.go
+++ token_source.go
@@ -6,6 +6,7 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"strings"
 	"time"
 
 	"golang.org/x/oauth2"
@@ -29,9 +30,6 @@ func NewFileTokenSource(context context.Context, clientId string, clientSecret s
 
 func (c *fileTokenSource) loadToken() (*oauth2.Token, error) {
 	log.Printf("[FTS] Loading token from file\n")
-	if c.configFilePath == "" {
-		return nil, fmt.Errorf("ScopedOAuth config file path can not be empty")
-	}
 
 	_, err := os.Stat(c.configFilePath)
 	if os.IsNotExist(err) {
@@ -75,12 +73,6 @@ func (c *fileTokenSource) saveToken(tok *oauth2.Token) error {
 }
 
 func (c *fileTokenSource) Token() (t *oauth2.Token, err error) {
-	// [ ] TODO: Address case when token expires before its end of life, because
-	// it's revoked.
-	// [ ] TODO: Addresss case when scopes are missing and token is not refreshed
-	// after scopes are corrected until config file is deleted.
-	// [ ] TODO: Address case when credentials are swaped and previous generated
-	// token is not refreshed untill config file is deleted.
 	t, _ = c.loadToken()
 	if t != nil && t.Valid() {
 		return t, nil
@@ -90,9 +82,128 @@ func (c *fileTokenSource) Token() (t *oauth2.Token, err error) {
 	if t, err = c.base.Token(); err != nil {
 		return nil, err
 	}
-	t.Expiry = t.Expiry.Add(86399 * time.Second)
 
 	err = c.saveToken(t)
 
 	return t, err
+}
+
+/*
+Token Source Implementation for Terraform Provider
+*/
+type tfprovTokenSource struct {
+	base           oauth2.TokenSource
+	configFilePath string
+	clientId       string
+	scopes         []string
+}
+
+type tfprovPersistentToken struct {
+	*oauth2.Token
+	ClientId string `json:"clientId"`
+	Scopes   string `json:"scopes"`
+}
+
+func NewTFProvFileTokenSource(context context.Context, clientId string, clientSecret string, scopes []string, configFilePath string) oauth2.TokenSource {
+	base := baseTokenSource(context, clientId, clientSecret, scopes)
+
+	fts := &tfprovTokenSource{
+		base:           base,
+		configFilePath: configFilePath,
+		clientId:       clientId,
+		scopes:         scopes,
+	}
+
+	return oauth2.ReuseTokenSource(nil, fts)
+}
+
+func (c *tfprovTokenSource) loadToken() (*oauth2.Token, error) {
+	log.Printf("[FTS] Loading token from file\n")
+
+	_, err := os.Stat(c.configFilePath)
+	if os.IsNotExist(err) {
+		return &oauth2.Token{}, nil
+	}
+
+	data, err := os.ReadFile(c.configFilePath)
+	if err != nil {
+		return nil, fmt.Errorf("failed to read file %s: %w", c.configFilePath, err)
+	}
+	pt := tfprovPersistentToken{}
+	if err := json.Unmarshal(data, &pt); err != nil {
+		return nil, fmt.Errorf("failed to decode content of %s: %w", c.configFilePath, err)
+	}
+
+	t := pt.Token
+
+	// [*] TODO: Persisted token is already expired in config file.
+	needToRefreshTokenExpired := time.Now().After(pt.Expiry)
+	// [*] TODO: Addresss case when scopes are missing and token is not refreshed
+	// after scopes are corrected until config file is deleted.
+	needToRefreshTokenNotSameScopes := !isSameScope(pt.Scopes, strings.Join(c.scopes, " "))
+	// [*] TODO: Address case when credentials are swaped and previous generated
+	// token is not refreshed untill config file is deleted.
+	needToRefreshTokenNotSameCredentials := pt.ClientId != c.clientId
+	if needToRefreshTokenExpired || needToRefreshTokenNotSameScopes || needToRefreshTokenNotSameCredentials {
+		t.AccessToken = ""
+	}
+
+	return t, nil
+}
+
+func (c *tfprovTokenSource) saveToken(tok *oauth2.Token) error {
+	log.Printf("[FTS] Saving token to file\n")
+
+	_, err := os.Stat(c.configFilePath)
+	if os.IsNotExist(err) {
+		if _, err := os.Create(c.configFilePath); err != nil {
+			return fmt.Errorf("failed to create file %s: %w", c.configFilePath, err)
+		}
+	}
+
+	p := tfprovPersistentToken{
+		Token:    tok,
+		ClientId: c.clientId,
+		Scopes:   strings.Join(c.scopes, " "),
+	}
+	data, err := json.Marshal(p)
+	if err != nil {
+		return fmt.Errorf("failed to encode token into file %s: %w", c.configFilePath, err)
+	}
+
+	if err := os.WriteFile(c.configFilePath, data, 0644); err != nil {
+		return fmt.Errorf("failed to save token into file %s: %w", c.configFilePath, err)
+	}
+
+	return nil
+}
+
+func (c *tfprovTokenSource) Token() (t *oauth2.Token, err error) {
+	// [*] TODO: Address case when token expires before its end of life, because
+	// it's revoked.
+	//    * This should be addressed in client when receiving the 401 response.
+	t, _ = c.loadToken()
+	if t != nil && t.Valid() {
+		return t, nil
+	}
+
+	log.Printf("[FTS] Fetching new token\n")
+	if t, err = c.base.Token(); err != nil {
+		return nil, err
+	}
+
+	err = c.saveToken(t)
+
+	return t, err
+}
+
+func isSameScope(a, b string) bool {
+	ta := strings.TrimSpace(a)
+	tb := strings.TrimSpace(b)
+
+	if strings.Compare(ta, tb) == 0 {
+		return true
+	}
+
+	return false
 }
