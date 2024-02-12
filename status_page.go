d0af0c2a0d91bdfefdfbd554e32641e38b0df1d1
Willem Potgieter <willempo@tfg.co.za>
2024-02-12 07:03:22 +0200

    Add ListStatusPageImpacts function

--- status_page.go
+++ status_page.go
@@ -13,10 +13,25 @@ type StatusPage struct {
 	Type           string
 }
 
+type StatusPageImpact struct {
+	ID          string
+	Self        string
+	Description string
+	PostType    string
+	StatusPage  StatusPage
+	Type        string
+}
+
 // ListStatusPagesResponse is the data structure returned from calling the ListStatusPages API endpoint.
 type ListStatusPagesResponse struct {
 	APIListObject
 	StatusPages []StatusPage `json:"status_pages"`
+}
+
+// ListStatusPageImpactsResponse is the data structure returned from calling the ListStatusPagesImpacts API endpoint.
+type ListStatusPageImpactsResponse struct {
+	APIListObject
+	StatusPageImpacts []StatusPageImpact `json:"impacts"`
 }
 
 // ListStatusPages lists the given types of status pages
@@ -30,6 +45,24 @@ func (c *Client) ListStatusPages(statusPageType string) (*ListStatusPagesRespons
 	}
 
 	var result ListStatusPagesResponse
+	if err := c.decodeJSON(resp, &result); err != nil {
+		return nil, err
+	}
+
+	return &result, nil
+}
+
+// ListStatusPageImpacts lists the given types of impacts for the specific status page
+func (c *Client) ListStatusPageImpacts(id string, postType string) (*ListStatusPageImpactsResponse, error) {
+	h := map[string]string{
+		"X-EARLY-ACCESS": "status-pages-early-access",
+	}
+	resp, err := c.get(context.Background(), "/status_pages/"+id+"/impacts?post_type="+postType, h)
+	if err != nil {
+		return nil, err
+	}
+
+	var result ListStatusPageImpactsResponse
 	if err := c.decodeJSON(resp, &result); err != nil {
 		return nil, err
 	}
