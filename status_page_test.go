1007d061363434b3452c771d5dac4100794ae290
Willem Potgieter <willempo@tfg.co.za>
2024-02-12 09:43:08 +0200

    Test for GetStatusPageImpact

--- status_page_test.go
+++ status_page_test.go
@@ -79,3 +79,34 @@ func TestStatusPage_ListImpacts(t *testing.T) {
 
 	testEqual(t, want, res)
 }
+
+// GetStatusPageImpact
+func TestStatusPage_GetImpact(t *testing.T) {
+	setup()
+	defer teardown()
+
+	mux.HandleFunc("/status_pages/1/impacts/1", func(w http.ResponseWriter, r *http.Request) {
+		testMethod(t, r, "GET")
+		_, _ = w.Write([]byte(`{"impact": {"id": "1","description":"Extreme","post_type":"incident","status_page":{"id": "1","name":"MyStatusPage","published_at":"2024-02-12T09:23:23Z","status_page_type":"public","url":"https://mypagerduty"}}}`))
+	})
+
+	client := defaultTestClient(server.URL, "foo")
+	res, err := client.GetStatusPageImpact("1", "1")
+	if err != nil {
+		t.Fatal(err)
+	}
+	want := &StatusPageImpact{
+		ID:          "1",
+		Description: "Extreme",
+		PostType:    "incident",
+		StatusPage: StatusPage{
+			ID:             "1",
+			Name:           "MyStatusPage",
+			PublishedAt:    "2024-02-12T09:23:23Z",
+			StatusPageType: "public",
+			URL:            "https://mypagerduty",
+		},
+	}
+
+	testEqual(t, want, res)
+}
